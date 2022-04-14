ecf9a6a8ab53dbffb1bcca1111d74e9eb3d27257
kaydan95 <kyori0515@gmail.com>
2022-04-14 21:24:06 +0900

    :pencil2: css 수정
    :pencil2: board 만드는 버튼 css 수정

--- src/Components/CreatingBoard.tsx
+++ src/Components/CreatingBoard.tsx
@@ -37,7 +37,7 @@ const CreateFromWrapper = styled.div`
     align-items: center;
     justify-content: center;
     padding : 20px;
-    overflow-x : hidden;
+    overflow : hidden;
     transition : width 0.3s linear;
     p {
         position: relative;
